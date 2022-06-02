231932ac1f2c8a7d6892210ffd0055e24894540f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-06-02 18:38:16 +0200

    feat: CCreating the points where the componets will beC

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import './App.css'
 
-function App() {
+function App(props) {
+
+  const Username: string = 'Here you need to put your username'
   
 
   return (
     <div className="App">
+       <div className="Container-Nav">
+          <div className="navbar">
+          <p> Navbar</p>
+          </div>
+        </div>
       <header className="App-header">
-        <nav>Navbar</nav>
+       
         <p>Hero</p>
         <h1>Header</h1>
       </header>
       <div className="container">
         <p>Hackathon</p>
-        <div> Section 1 </div>
+        <div> Section 1: Welcome {props.Username} 
+        </div>
+        <div className="About">
+          <h2>Here you can say something more about your project</h2>
+        </div>
         <div className="container">
+          <div className="card-deck">
           <div className="card">
             my Card 1
           </div>
@@ -24,15 +36,15 @@ function App() {
             my Card 3
           </div>
         </div>
+        </div>
         <footer>
           <div className="my-footer">
             <h3>
-              this is my footer
+              This is My Footer
             </h3>
           </div>
         </footer>
       </div>
-
     </div>
   )
 }
